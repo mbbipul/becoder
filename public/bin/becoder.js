5e4ffc18e36f11d7990cee3fdbe8e269a97aec15
Bipul <mbbipulmandol.cse3.bu@gmail.com>
2019-06-18 20:59:46 +0600

    Completing createContest backend and userContest showing in administration contest page

--- public/bin/becoder.js
+++ public/bin/becoder.js
@@ -1,7 +1,15 @@
 "use strict";
+var jumbotron = true;
+var auth = firebase.auth();
+var firebaseInitiatSuccesfull = false;
+
+var firestore = firebase.firestore();
+var settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+
 function signIn() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+    auth.signInWithPopup(provider);
     console.log("sing in");
 }
 
@@ -9,7 +17,6 @@ function signUpWithEmailPassword(){
   var email = userSignUpEmail.value;
   var username = userSignUpUsername.value;
   var password = userSignUpPass.value;
-  var auth = firebase.auth();
   auth.createUserWithEmailAndPassword(email,password).then(function() {
     // Sign-out successful.
     console.log("ahhh santi");
@@ -33,7 +40,7 @@ function signInWithEmailPassword(){
     var email = document.getElementById("user-sign-in-email").value;
     var password = document.getElementById("user-sign-in-password").value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    auth.signInWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -43,38 +50,110 @@ function signInWithEmailPassword(){
 }
 
 function signOut() {
-    firebase.auth().signOut();
+    auth.signOut();
 }
 
 // Initiate firebase auth.
-function initFirebaseAuth() {
-  var auth = firebase.auth();
+function initFirebaseAuth(jm) {
+  jumbotron = jm;
   auth.onAuthStateChanged(function(user) {
     authStateObserver(user);
   });
 }
 
+function initFirebaseAuthAdc() {
+  auth.onAuthStateChanged(function(user) {
+    getALlAdinistrationContest("manage_contest_lists");
+  });
+}
 // Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
   if(isUserSignedIn()){
-    return firebase.auth().currentUser.photoURL || '/assets/images/becoder.png';
+    return auth.currentUser.photoURL || '/assets/images/becoder.png';
   }
 }
 
 function getUserEmail(){
   if(isUserSignedIn()){
-    return firebase.auth().currentUser.email;
+    return auth.currentUser.email;
   }
 }
 // Returns the signed-in user's display name.
 function getUserName() {
   if(isUserSignedIn()){
-    return firebase.auth().currentUser.displayName;
+    return auth.currentUser.displayName;
   }
 }
-
+function getCurrentUserId() {
+  if(isUserSignedIn()){
+    return auth.currentUser.uid;
+  }
+}
 function isUserSignedIn() {
-      return !!firebase.auth().currentUser;
+      return !!auth.currentUser;
+}
+
+function getALlAdinistrationContest(elementId){
+  var contests = firestore.collection('contests').where('contestAuthorId', '==', getCurrentUserId());
+
+  firestore.collection("contests").where('contestAuthorId', '==', getCurrentUserId())
+      .onSnapshot(function(snapshot) {
+          snapshot.docChanges().forEach(function(change) {
+              if (change.type === "added") {
+                console.log("Modified city: ", change.doc.data());
+
+                var data = change.doc.data();
+
+                var tableRow = document.createElement("tr");
+                var tableRowHeader = document.createElement("th");
+                var tableRowDataCN = document.createElement("td");
+                var tableRowDataCO = document.createElement("td");
+                var tableRowDataSD = document.createElement("td");
+                var tableRowDataSU = document.createElement("td");
+                var tableRowDataPC = document.createElement("td");
+
+                tableRowHeader.setAttribute("scope","row");
+
+                var contestCount = document.createTextNode(1);
+                var contestNameText = document.createTextNode(data.contestName);
+                var contestOwnerText = document.createTextNode(data.organizationName);
+                var contestSatrtDate = document.createTextNode(data.startDate);
+                var ContestSignUp = document.createTextNode(100);
+                var ContestPartiCipants = document.createTextNode(30);
+
+                tableRowHeader.appendChild(contestCount);
+                tableRowDataCN.appendChild(contestNameText);
+                tableRowDataCO.appendChild(contestOwnerText);
+                tableRowDataSD.appendChild(contestSatrtDate);
+                tableRowDataSU.appendChild(ContestSignUp);
+                tableRowDataPC.appendChild(ContestPartiCipants);
+
+                tableRow.appendChild(tableRowHeader);
+                tableRow.appendChild(tableRowDataCN);
+                tableRow.appendChild(tableRowDataCO);
+                tableRow.appendChild(tableRowDataSD);
+                tableRow.appendChild(tableRowDataSU);
+                tableRow.appendChild(tableRowDataPC);
+
+
+                document.getElementById(elementId).appendChild(tableRow);
+                }
+              if (change.type === "modified") {
+                  console.log("Modified city: ", change.doc.data());
+              }
+              if (change.type === "removed") {
+                  console.log("Removed city: ", change.doc.data());
+              }
+          });
+      });
+
+  // contests.get().then(function (querySnapshot) {
+  //     var count = 1;
+  //     querySnapshot.forEach(function (doc) {
+  //
+  //     });
+  // });
+
 }
 
 function authStateObserver(user) {
@@ -90,8 +169,12 @@ function authStateObserver(user) {
     // Show user's profile and sign-out button.
     // Hide sign-in button.
     signInSignUpNavButton.setAttribute('hidden', 'true');
-    signInSignUpJumborton.setAttribute('hidden','true');
+    if(jumbotron){
+      signInSignUpJumborton.setAttribute('hidden','true');
+
+    }
     footerSigninSignupButton.setAttribute('hidden','true');
+    firebaseInitiatSuccesfull = true;
     // We save the Firebase Messaging Device token and enable notifications.
   } else { // User is signed out!
     // Hide user's profile and sign-out button.
@@ -99,7 +182,6 @@ function authStateObserver(user) {
     signInSignUpNavButton.removeAttribute('hidden');
     signInSignUpJumborton.removeAttribute('hidden');
     footerSigninSignupButton.removeAttribute('hidden');
-
   }
 }
 
